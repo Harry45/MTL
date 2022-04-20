1c160244b6dc60fe73e18f436adc2c0d35ee9ed7
Harry45 <arrykrish@gmail.com>
2022-04-20 10:41:01 +0100

    main file

--- main.py
+++ main.py
@@ -7,9 +7,23 @@ Description: Main script for the Galaxy Zoo project.
 # Email: arrykrish@gmail.com/a.mootoovaloo17@imperial.ac.uk/arrykrishna.mootoovaloo@physics.ox.ac.uk
 # Project: Multi-Task Learning for Galaxy Zoo
 
+import os
+import torch
+from torch.utils.data import DataLoader
+from torch.utils.tensorboard import SummaryWriter
+
+# our scripts and functions
+from src.dataset import DECaLSDataset
 import utils.helpers as hp
 import src.processing as sp
 import settings as st
+
+# create an output directory
+OUT_PATH = './output/'
+os.makedirs(OUT_PATH, exist_ok=True)
+
+# Set device to CUDA if a CUDA device is available, else CPU
+DEVICE = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 
 def splitting(val_size: float = 0.2, test_size: float = 0.2) -> None:
@@ -29,6 +43,15 @@ def splitting(val_size: float = 0.2, test_size: float = 0.2) -> None:
 
     # train, validate, test
     _ = sp.split_data(labels, val_size, test_size, save=True)
+
+
+def training(batch_size: int = 8, epochs: int = 10, lr: float = 0.001):
+
+    train_dataset = DECaLSDataset(mode='train', augment=True)
+    valid_dataset = DECaLSDataset(mode='validate', augment=False)
+
+    train_dataloader = DataLoader(train_dataset, batch_size=batch_size, drop_last=True)
+    valid_dataloader = DataLoader(valid_dataset, batch_size=batch_size)
 
 
 if __name__ == '__main__':
