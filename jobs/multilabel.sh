f7053f39fe9834d4972a414415dd6d4aba8d04eb
Arrykrishna Mootoovaloo <arrykrish@gmail.com>
2022-04-28 17:21:49 +0100

    jobs

--- jobs/multilabel.sh
+++ jobs/multilabel.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #SBATCH --mail-type=BEGIN,END
 #SBATCH --mail-user=arrykrish@gmail.com
+#SBATCH --no-requeue
 #SBATCH --time=23:30:00
 #SBATCH --job-name=multi-label-network
 #SBATCH --ntasks-per-node=1
 #SBATCH --mem-per-cpu=32G
-#SBATCH --partition=long
 #SBATCH --cluster=htc
 #SBATCH --gres=gpu:1
 #SBATCH --output=jobs/%j.out
