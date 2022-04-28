8f14dd551d67dd26f648ef84760f916419057ace
Arrykrishna Mootoovaloo <arrykrish@gmail.com>
2022-04-28 13:34:56 +0100

    training

--- train_ML.py
+++ train_ML.py
@@ -10,12 +10,18 @@ Description: Train the network for the multi-label case.
 import os
 import torch
 import torch.nn as nn
-from src.dataset import DECaLSDataset
 from torch.utils.data import Dataset, DataLoader
+from torch.utils.tensorboard import
+
+# our scripts and functions
 from src.network import MultiLabelNet
+from src.dataset import DECaLSDataset
 
 out_path = './output/'
 os.makedirs(out_path, exist_ok=True)
+
+# Set device to CUDA if a CUDA device is available, else CPU
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 # create the dataloader
 train_dataset = DECaLSDataset(mode='train', augment=False)
@@ -26,6 +32,63 @@ val_loader = DataLoader(dataset=val_loader, batch_size=4, shuffle=False)
 
 # define the model
 model = MultiLabelNet(backbone="resnet18")
+model.to(device)
 
 # to assign weights to this loss function
-loss_func = nn.MultiLabelSoftMarginLoss(weight=st.CLASS_WEIGHTS, reduction='mean')
+weights = torch.tensor(st.CLASS_WEIGHTS)
+
+# set the optimizer
+optimizer = torch.optim(model.parameters(), lr=1E-4, weight_decay=1E-5)
+criterion = nn.MultiLabelSoftMarginLoss(weight=weights, reduction='mean')
+
+writer = SummaryWriter(os.path.join(out_path, "summary"))
+
+epochs = 2
+
+for epoch in range(epochs):
+    print("[{} / {}]".format(epoch + 1, epochs))
+    model.train()
+
+    losses = []
+
+    # Training Loop Start
+    for images, targets in train_dataloader:
+        images, targets = map(lambda x: x.to(device), [imgages, targets])
+
+        outputs = model(images)
+        loss = criterion(outputs, targets)
+
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        losses.append(loss.item())
+
+    train_loss = sum(losses) / len(losses)
+    writer.add_scalar('train_loss', train_loss, epoch)
+
+    print(f"Training: Loss={train_loss:.2f}")
+    # Training Loop End
+
+    # Evaluation Loop Start
+    model.eval()
+
+    losses = []
+
+    for images, targets in val_dataloader:
+        imgages, targets = map(lambda x: x.to(device), [images, targets])
+
+        outputs = model(images)
+        loss = criterion(outputs, targets)
+
+        losses.append(loss.item())
+
+    val_loss = sum(losses) / max(1, len(losses))
+    writer.add_scalar('val_loss', val_loss, epoch)
+
+    print(f"Validation: Loss={val_loss:.2f}")
+    print("-"*25)
+
+# model_path = '../fs-models/'
+# os.makedirs(model_path, exist_ok=True)
+# torch.save(model.state_dict(), model_path + 'resnet_18_multilabel.pth')
