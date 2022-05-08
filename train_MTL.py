772dfb2c2d7e80353232ffcea10897e4f6f7eded
Harry45 <arrykrish@gmail.com>
2022-05-08 11:35:56 +0100

    training multi-task learning network

---
+++ train_MTL.py
@@ -0,0 +1,108 @@
+"""
+Description: Train the network for the multi-task case.
+"""
+
+# Author: Arrykrishna Mootoovaloo
+# Date: April 2022
+# Email: arrykrish@gmail.com/a.mootoovaloo17@imperial.ac.uk/arrykrishna.mootoovaloo@physics.ox.ac.uk
+# Project: Multi-Task Learning for Galaxy Zoo
+
+import os
+import torch
+import torch.nn as nn
+from torch.utils.data import Dataset, DataLoader
+from torch.utils.tensorboard import SummaryWriter
+
+# our scripts and functions
+from src.network import MultiTaskNet
+from src.dataset import DECaLSDataset
+import settings as st
+
+out_path = './output/'
+model_path = '../ml-models/'
+
+# make the folders
+os.makedirs(out_path, exist_ok=True)
+os.makedirs(model_path, exist_ok=True)
+
+# Set device to CUDA if a CUDA device is available, else CPU
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+# create the dataloader
+train_dataset = DECaLSDataset(mode='train', augment=False, multi_task=True)
+val_dataset = DECaLSDataset(mode='validate', augment=False, multi_task=True)
+
+train_loader = DataLoader(dataset=train_dataset, batch_size=64, shuffle=True)
+val_loader = DataLoader(dataset=val_dataset, batch_size=64, shuffle=False)
+
+# define the model
+model = MultiTaskNet(backbone="resnet18", output_size=st.LABELS_PER_TASK)
+model.to(device)
+
+# set the optimizer
+optimizer = torch.optim.Adam(model.parameters(), lr=1E-4, weight_decay=1E-5)
+loss_func = nn.BCEWithLogitsLoss()
+
+
+def criterion(loss_function, outputs: nn.ModuleDict, labels: dict) -> torch.tensor:
+    losses = 0
+
+    # we can add a weight for each loss
+    # for example, we can calculate the fraction of objects which go into Nodes
+    # 2, 3, 4 and so forth and then weight the loss accordingly
+    for i, key in enumerate(outputs):
+        losses += loss_function(outputs[key], labels[key].float().to(device))
+    return losses
+
+
+writer = SummaryWriter(os.path.join(out_path, "summary"))
+
+epochs = 5
+
+for epoch in range(epochs):
+    print("Epoch [{} / {}]".format(epoch + 1, epochs))
+    model.train()
+
+    losses = []
+
+    # Training Loop Start
+    for images, targets in train_loader:
+
+        images = images.to(device)
+        # images, targets = map(lambda x: x.to(device), [images, targets])
+
+        outputs = model(images)
+        loss = criterion(loss_func, outputs, targets)
+
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        losses.append(loss.item())
+
+    train_loss = sum(losses) / len(losses)
+    writer.add_scalar('train_loss', train_loss, epoch)
+
+    print(f"Training   : Loss={train_loss:.2e}")
+    # Training Loop End
+
+    # Evaluation Loop Start
+    model.eval()
+
+    losses = []
+
+    for images, targets in val_loader:
+        images, targets = map(lambda x: x.to(device), [images, targets])
+
+        outputs = model(images)
+        loss = criterion(loss_func, outputs, targets)
+
+        losses.append(loss.item())
+
+    val_loss = sum(losses) / len(losses)
+    writer.add_scalar('val_loss', val_loss, epoch)
+
+    print(f"Validation : Loss={val_loss:.2e}")
+    print("-" * 30)
+
+    # torch.save(model.state_dict(), model_path + 'resnet_18_multilabel_' + str(epoch) + '.pth')
