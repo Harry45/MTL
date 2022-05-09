e9a5b2d5a3c01bc0a3dba6b406446c26b7c22432
Arrykrishna Mootoovaloo <arrykrish@gmail.com>
2022-05-09 15:55:57 +0100

    predictions

--- prediction_MTL.py
+++ prediction_MTL.py
@@ -36,8 +36,6 @@ def predict_labels(output: nn.ModuleDict) -> dict:
 
         # predictions for the i^th task (logits)
         logits = output['task_' + str(i + 1)]
-        logits = logits.clone()
-        print(logits)
 
         # convert to probability
         probabilities = torch.nn.functional.softmax(logits, dim=1)
