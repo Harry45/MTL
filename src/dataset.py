f630698da213973b3f6b0de44c5c894e0a97f252
Harry45 <arrykrish@gmail.com>
2022-05-18 10:27:24 +0100

    dataset

--- src/dataset.py
+++ src/dataset.py
@@ -36,22 +36,22 @@ class DECaLSDataset(Dataset):
             self.desc = hp.load_csv(path, 'train')
             print(f'The number of training points is {self.desc.shape[0]}')
 
-            # to remove later
-            self.desc = self.desc.iloc[0:500]
+            # to remove later (this is for short experiments)
+            # self.desc = self.desc.iloc[0:500]
 
         elif mode == 'test':
             self.desc = hp.load_csv(path, 'test')
             print(f'The number of test points is {self.desc.shape[0]}')
 
-            # to remove later
-            self.desc = self.desc.iloc[0:200]
+            # to remove later (this is for short experiments)
+            # self.desc = self.desc.iloc[0:200]
 
         else:
             self.desc = hp.load_csv(path, 'validate')
             print(f'The number of validation points is {self.desc.shape[0]}')
 
-            # to remove later
-            self.desc = self.desc.iloc[0:200]
+            # to remove later (this is for short experiments)
+            # self.desc = self.desc.iloc[0:200]
 
         # transformations
         trans = st.TRANS
