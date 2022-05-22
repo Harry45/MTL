2edf54d426fcda39ba9513bcbd820ca9ef968342
Harry45 <arrykrish@gmail.com>
2022-05-22 09:04:07 +0100

    main setting file

--- settings.py
+++ settings.py
@@ -1,5 +1,5 @@
 """
-Description: Main file contain the main settings for running the codes. 
+Description: This file contain the main settings for running the codes. 
 """
 
 # Author: Arrykrishna Mootoovaloo
