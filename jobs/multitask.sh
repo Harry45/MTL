d9e6eb6313bec39b5dc9055d14f9ec4ff3d6c27e
Harry45 <arrykrish@gmail.com>
2022-05-24 04:44:25 +0100

    training for multitask

--- jobs/multitask.sh
+++ jobs/multitask.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 #SBATCH --mail-type=BEGIN,END
 #SBATCH --mail-user=arrykrish@gmail.com
-#SBATCH --time=00:30:00
+#SBATCH --time=125:00:00
 #SBATCH --job-name=multitask
 #SBATCH --ntasks-per-node=1
 #SBATCH --mem-per-cpu=32G
-#SBATCH --partition=short
+#SBATCH --partition=long
 #SBATCH --cluster=htc
-#SBATCH --gres=gpu:1
+#SBATCH --gres=gpu:v100:4
 #SBATCH --output=jobs/%j.out
 
 echo Checking GPU
@@ -29,5 +29,3 @@ time python train_MTL.py
 
 echo Training completed.
 date "+%H:%M:%S   %d/%m/%y"
-
-gp multitask job
