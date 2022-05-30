8693d99a526a5f2a288c04d5660fd1e09d3cff1f
Arrykrishna Mootoovaloo <arrykrish@gmail.com>
2022-05-30 15:44:10 +0100

    training

--- train_ML.py
+++ train_ML.py
@@ -48,12 +48,11 @@ model.to(device)
 optimizer = torch.optim.Adam(model.parameters(), lr=1E-4, weight_decay=1E-5)
 
 # to assign weights to this loss function
-# weights = torch.tensor(st.CLASS_WEIGHTS).to(device)
+weights = torch.tensor(st.CLASS_WEIGHTS).to(device)
 
 # loss function
 # criterion = nn.MultiLabelSoftMarginLoss(weight=weights, reduction='mean')
-# criterion = nn.BCEWithLogitsLoss(weight=weights, reduction='mean')
-criterion = nn.BCEWithLogitsLoss(reduction='mean')
+criterion = nn.BCEWithLogitsLoss(weight=weights, reduction='mean')
 
 writer = SummaryWriter(os.path.join(out_path, "summary"))
 
