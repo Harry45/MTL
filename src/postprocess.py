1b96fd3ded37937f070ece3c5d0a3e47826ba15d
Harry45 <arrykrish@gmail.com>
2022-08-15 08:32:37 +0100

    build tree

--- src/postprocess.py
+++ src/postprocess.py
@@ -8,12 +8,15 @@ Description: This file is for calculating the evaluation metrics and other quant
 # Project: Multi-Task Learning for Galaxy Zoo
 
 import os
+import torch
 from typing import Tuple
 import numpy as np
 import pandas as pd
+import matplotlib.pylab as plt
 from sklearn.metrics import hamming_loss, accuracy_score, f1_score, precision_score, recall_score
 
 # our scripts and functions
+import src.processing as sp
 import utils.helpers as hp
 import settings as st
 
@@ -193,3 +196,132 @@ def metric_multitask(predictions: str, ground_truth: str, save: bool = False) ->
         hp.save_csv(metrics_task, folder, 'MTL_metrics_task')
 
     return metrics_class, metrics_task
+
+
+def compare_labels_ml(
+        dataloader: torch.utils.data.DataLoader, idx: int, pred: pd.DataFrame, savefig: bool = False) -> Tuple[
+        np.ndarray, np.ndarray]:
+    """Compare the predicted labels with the ground truth for the multi-label
+    learning case.
+
+    Args:
+        dataloader (torch.utils.data.DataLoader): The dataloader containing the data.
+        idx (int): The index of the image to be compared.
+        pred (pd.DataFrame): The predictions.
+        savefig (bool, optional): Option to save the figure. Defaults to False.
+
+    Returns:
+        Tuple[np.ndarray, np.ndarray]: The predicted and ground truth labels.
+    """
+
+    # the labels
+    columns = np.array(dataloader.dataset.desc.columns[2:])
+
+    # the filename
+    filename = dataloader.dataset.desc.iauname.iloc[idx]
+
+    # the data
+    data = dataloader.dataset[idx]
+
+    # the predicted labels
+    labels_pred = columns[pred.iloc[idx].values == 1]
+
+    # the ground truth
+    labels_test = columns[data[1] == 1]
+
+    name = 'copper'
+    plt.figure(figsize=(4, 4))
+    plt.imshow(data[0].permute(1, 2, 0), cmap=plt.get_cmap(name))
+    plt.axis('off')
+
+    if savefig:
+        os.makedirs('plots', exist_ok=True)
+        plt.savefig(f'plots/{filename}.pdf', bbox_inches='tight')
+    plt.show()
+
+    print('Predictions')
+    print('-' * 11)
+    for i, element in enumerate(labels_pred):
+        print(f'Tag {i:2d}: {element}')
+
+    print()
+    print('Volunteers')
+    print('-' * 10)
+    for i, element in enumerate(labels_test):
+        print(f'Tag {i:2d}: {element}')
+
+    return labels_test, labels_pred
+
+
+def labels_per_task(dataframe: pd.DataFrame, index: int = 0) -> dict:
+    """Creates a dictionary where the keys are the tasks and the values are the
+    numeric values (0 or 1).
+
+    Args:
+        dataframe (pd.DataFrame): a dataframe with the labels.
+        index (int, optional): the row we want to choose from the dataframe. Defaults to 0.
+
+    Returns:
+        dict: a dictionary where each task has a list of labels, for example,
+        {'task_1': [0, 1, 0]}
+    """
+
+    # the first two column names are the name and png locations
+    labels = dataframe.iloc[index, 2:]
+
+    label_dict = dict()
+    for i in range(st.NUM_TASKS):
+        task = labels[st.LABELS['task_' + str(i + 1)]].values.astype(int)
+        label_dict['task_' + str(i + 1)] = task
+
+    return label_dict
+
+
+def build_tree(test_set: pd.DataFrame, pred_set: pd.DataFrame,
+               save: bool, fname: str) -> Tuple[pd.DataFrame, pd.DataFrame]:
+    """Builds a tree for the multitask learning case.
+
+    Args:
+        test_set (pd.DataFrame): The ground truth.
+        pred_set (pd.DataFrame): The predictions.
+        save (bool): Whether to save the tree.
+        fname (str): The name of the file to save the tree.
+
+    Returns:
+        Tuple[pd.DataFrame, pd.DataFrame]: The ground truth and predictions.
+    """
+
+    # number of test point
+    ntest = test_set.shape[0]
+
+    # create empty lists to store the trees
+    tree_pred = list()
+    tree_test = list()
+
+    for idx in range(ntest):
+
+        # re-write the test point in a dictionary (label per task)
+        test_point = labels_per_task(test_set, idx)
+
+        # find the labels (ground truths and test point)
+        pred_labels = sp.find_labels(pred_set[idx])
+        test_labels = sp.find_labels(test_point)
+
+        # record the trees
+        tree_pred.append(pred_labels)
+        tree_test.append(test_labels)
+
+    # generate the dataframe with the trees
+    tree_pred_df = pd.concat(tree_pred)
+    tree_test_df = pd.concat(tree_test)
+
+    # reset the indices
+    tree_pred_df.reset_index(drop=True, inplace=True)
+    tree_test_df.reset_index(drop=True, inplace=True)
+
+    # store the trees
+    if save:
+        hp.save_pd_csv(tree_pred_df, 'results', f'tree_pred-{fname}')
+        hp.save_pd_csv(tree_test_df, 'results', f'tree_test-{fname}')
+
+    return tree_pred_df, tree_test_df
